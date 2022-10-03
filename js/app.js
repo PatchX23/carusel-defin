e4d7455bd2fe6e3269ada239518b5492aa64ae55
Xavi <luisangelxavi1@gmail.com>
2022-10-03 11:03:27 -0400

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__lista'), { 
+slidesToShow: 4,
+slidesToScroll: 4,
+dost: '.carousel__indicadores',
+arrows: {
+    prev: '.carousel__anterior',
+    next: '.carousel__siguiente'
+}
+
+
+    });
+
+
+});
